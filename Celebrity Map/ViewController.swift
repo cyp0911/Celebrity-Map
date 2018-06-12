7cb64e04555c54dacc98f206be7d15b2e2e28686
Yinpeng Chen <cyp0911@gmail.com>
2018-06-12 18:35:24 -0300

    Initial Commit

---
+++ Celebrity Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Celebrity Map
+//
+//  Created by Yinpeng Chen on 2018-06-12.
+//  Copyright © 2018 Yinpeng Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
